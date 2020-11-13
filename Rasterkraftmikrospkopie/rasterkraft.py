5548304b3cdf43b4ab78c2f3d2b1cdb9518c9ebb
SagruH <sagru2899@googlemail.com>
2020-11-13 18:29:41 +0100

    Rasterkraft ini

---
+++ Rasterkraftmikrospkopie/rasterkraft.py
@@ -0,0 +1,14 @@
+import numpy as np
+import PhyPraKit as ppk #wurde von mir verändert
+import matplotlib.pyplot as plt
+
+from scipy import stats
+from scipy import constants as const
+from scipy.interpolate import make_interp_spline, BSpline
+
+#import kafe
+#from kafe.function_tools import FitFunction, LaTeX, ASCII
+#from kafe.function_library import quadratic_3par
+
+import uncertainties as uc
+from uncertainties.umath import sqrt
