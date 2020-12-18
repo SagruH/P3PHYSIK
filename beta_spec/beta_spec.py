a276e7a1d8bc27851a8b2e43a3271dcbc3c361b0
SagruH <sagru2899@googlemail.com>
2020-12-19 00:45:54 +0100

    beta spec read in data

---
+++ beta_spec/beta_spec.py
@@ -0,0 +1,17 @@
+import numpy as np
+import PhyPraKit as ppk #wurde von mir verändert
+import matplotlib.pyplot as plt
+
+from scipy import stats
+from scipy import constants as const
+
+import uncertainties as uc
+from uncertainties.umath import sqrt, log
+
+
+def main():
+    [A, c, t] = np.loadtxt("gruppe115_daten/beta_spectrum.txt", unpack = True)
+    
+    return;
+
+main()
