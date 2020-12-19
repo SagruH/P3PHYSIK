bd5cb51ffc1d89f35c73efb5548248f347761f7c
SagruH <sagru2899@googlemail.com>
2020-12-19 01:05:09 +0100

    beta spec background

--- beta_spec/beta_spec.py
+++ beta_spec/beta_spec.py
@@ -10,8 +10,24 @@ from uncertainties.umath import sqrt, log
 
 
 def main():
-    [A, c, t] = np.loadtxt("gruppe115_daten/beta_spectrum.txt", unpack = True)
-    
+    [A, c, mt] = np.loadtxt("gruppe115_daten/beta_spectrum.txt", unpack = True)
+    mt = 90 #s
+
+    A_back = A[:7]
+    A = A[7:]
+    c_back = c[:7]
+    c = c[7:]
+
+    background_counts = np.sum(c_back)/len(c_back)
+    print("background counts average:  ", background_counts)
+
+    plt.plot(A,c, "xr")
+    plt.plot(A,c, "-b")
+    plt.xlabel("Stromstärke in A")
+    plt.ylabel("Gezählte Ereignisse")
+    plt.grid(True)
+    #plt.show()
+
     return;
 
 main()
