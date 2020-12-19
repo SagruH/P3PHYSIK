28949430366f98f2e8e10ca18499d20b25800718
SagruH <sagru2899@googlemail.com>
2020-12-19 21:19:03 +0100

    rm phypra kit

--- beta_spec/beta_spec.py
+++ beta_spec/beta_spec.py
@@ -1,5 +1,4 @@
 import numpy as np
-import PhyPraKit as ppk #wurde von mir verändert
 import matplotlib.pyplot as plt
 
 from scipy import stats
@@ -8,18 +7,30 @@ from scipy import constants as const
 import uncertainties as uc
 from uncertainties.umath import sqrt, log
 
+def aufgabe1_mag():
+    data = np.loadtxt("daten.txt", delimiter = ",", skiprows = 1, unpack = True)
+
+    return;
+
 
 def main():
+    aufgabe1_mag()
+
     [A, c, mt] = np.loadtxt("gruppe115_daten/beta_spectrum.txt", unpack = True)
     mt = 90 #s
 
+    #background radiation
     A_back = A[:7]
     A = A[7:]
     c_back = c[:7]
     c = c[7:]
-
     background_counts = np.sum(c_back)/len(c_back)
     print("background counts average:  ", background_counts)
+    # corect for backgroudn radiation
+    c -= background_counts
+
+
+
 
     plt.plot(A,c, "xr")
     plt.plot(A,c, "-b")
