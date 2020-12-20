2d94e532fe990dd21c485f3bf963aceaa7d54322
SagruH <sagru2899@googlemail.com>
2020-12-20 02:06:14 +0100

    beta spec aufgabe 1 und corections

--- beta_spec/beta_spec.py
+++ beta_spec/beta_spec.py
@@ -8,13 +8,27 @@ import uncertainties as uc
 from uncertainties.umath import sqrt, log
 
 def aufgabe1_mag():
-    data = np.loadtxt("daten.txt", delimiter = ",", skiprows = 1, unpack = True)
+    data = np.loadtxt("daten.csv", delimiter = ",", skiprows = 1, unpack = True)
+    I = data[0]
+    B = data[1]
 
+    slp, b, r_v, p_v, std_err = stats.linregress(I, B)
+    xr = np.linspace(0,10, 350)
+
+    print("m :",slp ," b: ", b)
+    plt.plot(I,B, "xb")
+    plt.plot(xr, slp*xr+b, "-r")
+
+    plt.xlabel("I in A")
+    plt.ylabel("B in mT")
+    plt.grid(True)
+    plt.show()
     return;
 
 
+
 def main():
-    aufgabe1_mag()
+    #aufgabe1_mag()
 
     [A, c, mt] = np.loadtxt("gruppe115_daten/beta_spectrum.txt", unpack = True)
     mt = 90 #s
@@ -25,19 +39,20 @@ def main():
     c_back = c[:7]
     c = c[7:]
     background_counts = np.sum(c_back)/len(c_back)
-    print("background counts average:  ", background_counts)
-    # corect for backgroudn radiation
-    c -= background_counts
+
+    # corecttions
+    Z = (968/1000) * (c-background_counts) / A
 
 
 
-
-    plt.plot(A,c, "xr")
-    plt.plot(A,c, "-b")
-    plt.xlabel("Stromstärke in A")
-    plt.ylabel("Gezählte Ereignisse")
-    plt.grid(True)
-    #plt.show()
+    if 1:
+        print("background counts average:  ", background_counts)
+        plt.plot(A,Z, "xr")
+        plt.plot(A,Z, "-b")
+        plt.xlabel("Stromstärke in A")
+        plt.ylabel("Gezählte Ereignisse")
+        plt.grid(True)
+        plt.show()
 
     return;
 
