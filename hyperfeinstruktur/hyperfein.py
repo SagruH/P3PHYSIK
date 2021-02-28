1bd1efe323a245ef71011005e62c2d9e3b08e03f
SagruH <sagru2899@googlemail.com>
2021-02-28 16:46:25 +0100

    Hyperfeinstruktur uv mehr data

--- hyperfeinstruktur/hyperfein.py
+++ hyperfeinstruktur/hyperfein.py
@@ -45,6 +45,7 @@ def uc_lin_reg(x,xerr,y,yerr):
     b = ym - m*xm
     return m,b;
 
+
 def laser():
     data1 = np.loadtxt("115/laser_green1.txt",unpack=True)
     data2 = np.loadtxt("115/laser_green2.txt",unpack=True)
@@ -83,6 +84,7 @@ def laser():
     #plt.show()
     plt.clf()
     return d;
+
 
 def green(d):
     data1 = np.loadtxt("115/data_green1.txt",unpack=True)
@@ -143,8 +145,7 @@ def green(d):
     return;
 
 
-
-def uv(d):
+def uv_old(d):
     data0 = np.loadtxt("115/date_uv0.txt",unpack=True)
     data1 = np.loadtxt("115/date_uv1.txt",unpack=True)
     data2 = np.loadtxt("115/date_uv2.txt",unpack=True)
@@ -220,10 +221,83 @@ def uv(d):
     plt.clf()
     return;
 
+def uv(d):
+    data0 = np.loadtxt("115/date_uv0.txt",unpack=True)
+    data1 = np.loadtxt("115/date_uv1.txt",unpack=True)
+    data2 = np.loadtxt("115/date_uv2.txt",unpack=True)
+    data3 = np.loadtxt("115/date_uv3.txt",unpack=True)
+    data4 = np.loadtxt("115/date_uv4.txt",unpack=True)
+    data5 = np.loadtxt("115/date_uv5.txt",unpack=True)
+
+    f   = uc.ufloat(150,1)*1e-3
+
+    d1a = 2*( np.array([93,161,244,342,1048,1110])-695    ) /155
+    d1c = 2*( np.array([120,196,281,400,1000,1091])-695    ) /155
+    d1i = 2*( np.array([139,214,308,432,967,1087] )-695    ) /155
+
+    d5a = 2*(np.array([95,178,251,349,1095,1222])-722) /155
+    d5c = 2*(np.array([117,207,289,394,1044,1177])-722) /155
+    d5i = 2*(np.array([154,225,314,426,995,1138])-722)  /155
+
+    dma = np.mean([d1a,d5a],axis = 0) **2
+    dmc = np.mean([d1c,d5c],axis = 0) **2
+    dmi = np.mean([d1i,d5i],axis = 0) **2
+    derra = np.std([d1a,d5a],axis = 0) **2
+    derrc = np.std([d1c,d5c],axis = 0) **2
+    derri = np.std([d1i,d5i],axis = 0) **2
+
+    ma,ba = uc_lin_reg([4,3,2,1,1,2],0,dma,derra)
+    mc,bc = uc_lin_reg([4,3,2,1,1,2],0,dmc,derrc)
+    mi,bi = uc_lin_reg([4,3,2,1,1,2],0,dmi,derri)
+
+    lama = (1e-6*ma*d)/(4*f**2)
+    lamc = (1e-6*mc*d)/(4*f**2)
+    lami = (1e-6*mi*d)/(4*f**2)
+
+    lamq = (lama+ lamc+ lami)/3
+
+    Dlamac = np.mean(lamq/(8*f**2) * ((dma-dmc)*1e-6))
+    Dlamai = np.mean(lamq/(8*f**2) * ((dma-dmi)*1e-6))
+    Dlamci = np.mean(lamq/(8*f**2) * ((dmc-dmi)*1e-6))
+
+
+    #Data Print ---------------------------
+    print("Außen in mm²: m = ", ma , "b = ", ba)
+    print("mitte in mm²: m = ", mc , "b = ", bc)
+    print("Innen in mm²: m = ", mi , "b = ", bi)
+    print("lambda außen = ", lama*1e9, "nm")
+    print("lambda mitte = ", lamc*1e9, "nm")
+    print("lambda innen = ", lami*1e9, "nm")
+    print(Dlamac/lamq, Dlamai/lamq, Dlamci/lamq)
+
+    #plt.plot(data0[0], data0[1], "m.-",label="Data0")   #nope
+    #plt.plot(data1[0], data1[1], "b.-",label="Data1")   #good
+    #plt.plot(data2[0], data2[1], "r.-",label="Data2")   #maybe (nope)
+    #plt.plot(data3[0], data3[1], "g.-",label="Data3")   #nope
+    #plt.plot(data4[0], data4[1], "y.-",label="Data4")   #good
+    #plt.plot(data5[0], data5[1], "k.-",label="Data5")   #good
+
+    xw = np.linspace(0.5,4.2,1000)
+    plt.plot([4,3,2,1,1,2],dma,"bo", label = "Data Außen")
+    plt.plot([4,3,2,1,1,2],dmi,"co", label = "Data Innen")
+    plt.plot([4,3,2,1,1,2],dmc,"ko", label = "Data Mitte")
+    plt.plot(xw,ma.n*xw+ba.n,"r-", label = ("Fit Außen: ("+ str(ma) + ") * x + (" + str(ba) + ")") )
+    plt.plot(xw,mc.n*xw+bc.n,"y-", label = ("Fit Mitte: ("+ str(mc) + ") * x + (" + str(bc) + ")") )
+    plt.plot(xw,mi.n*xw+bi.n,"m-", label = ("Fit Innen: ("+ str(mi) + ") * x + (" + str(bi) + ")") )
+
+    plt.xlabel("Ordnung n")
+    plt.ylabel("Durchmesser D² in mm²")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    plt.clf()
+    return;
+
 
 def main():
     d = laser()
     #green(d)
+    uv_old(d)
     uv(d)
     return;
 
